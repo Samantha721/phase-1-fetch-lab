651100c7fc777890f7b590bdace8dffee2edc7c9
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-30 22:53:50 -0700

    commit tree 346bc46d7a826682b85747eb81f12737fa9600cd

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+//   fetch("https://anapioficeandfire.com/api/books")
+//     .then((res) => res.json())
+//     .then((data) => renderBooks(data));
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector("main");
+//   books.forEach((book) => {
+//     const h2 = document.createElement("h2");
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books");
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((data) => renderBooks(data));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+function renderBooks(datas) {
+  const main = document.querySelector("main");
+  datas.forEach((data) => {
+    const h2 = document.createElement("h2");
+    h2.textContent = data.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", () => {
   fetchBooks();
 });
